9b04f28bdd7f304797c29d8ae273c24e7bb8999f
original <git@github.com:denis-poroshin/variables.git>
2024-02-22 20:25:44 +0300

    homework done ver. 2.0

--- src/Main.java
+++ src/Main.java
@@ -12,53 +12,65 @@ public class Main {
         if (year < 1584){
             System.out.println("Данные заведены не корректно");
         }
-        String leapYear = highYearCalculation(year);
-        System.out.println(year + leapYear);
+        boolean leapYear = definitionOfHighYear(year);
+        if (leapYear){
+            System.out.println(year + " год — високосный год");
+        }else{
+            System.out.println(year + " год — невисокосный год");
+        }
     }
-    public static String highYearCalculation(int year){
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0 && year > 1584)){
-            return " год — високосный год";
-        }else
-            return " год — невисокосный год";
+    public static boolean definitionOfHighYear(int year){
+        boolean flag;
+        if (year > 1584 && year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+            return flag = true;
+        }else {
+            return flag = false;
+
+        }
     }
     public static void task2(){
         System.out.println("Задание 2");
         int operatingSystemType = 0;
-        int clientDeviceYear = LocalDate.now().getYear();
-        gettingTheApplicationVersion(operatingSystemType, clientDeviceYear); // Если делать через возрат значений, то я как понял нужно будет два метода?
-    }
-    public static void gettingTheApplicationVersion(int gettingTheApplicationVersion, int clientDeviceYear){ // Хотел прописать через String, а не через void, но нужно как-то вернуть два занчения, а это только через два метода и передовать по одной переменной
+        int yearOfDevice = 2024;
 
-        if (gettingTheApplicationVersion == 0 && clientDeviceYear > 2015) {
+        printInfoAboutVersion(operatingSystemType, yearOfDevice);
+    }
+    public static void printInfoAboutVersion(int gettingTheApplicationVersion, int yearOfDevice){
+        int clientDeviceYear = LocalDate.now().getYear();
+        if (gettingTheApplicationVersion == 0 && yearOfDevice >= clientDeviceYear) {
             System.out.println("Установите приложение для iOS");
-        }else if(gettingTheApplicationVersion == 0 && clientDeviceYear < 2015) {
+        }else if(gettingTheApplicationVersion == 0 && yearOfDevice < clientDeviceYear) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        }else if(gettingTheApplicationVersion == 1 && clientDeviceYear > 2015){
+        }else if(gettingTheApplicationVersion == 1 && yearOfDevice >= clientDeviceYear){
             System.out.println("Установите приложение для Android");
-        }else if (gettingTheApplicationVersion == 1 && clientDeviceYear < 2015){
+        }else if (gettingTheApplicationVersion == 1 && yearOfDevice < clientDeviceYear){
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
     }
     public static void task3(){
         System.out.println("Задание 3");
         int deliveryDistance = 95;
-        calculationOfDaysForDelivery(deliveryDistance);
+        int amountOfDays = calculationOfDaysForDelivery(deliveryDistance);
+        if (amountOfDays > 0){
+            System.out.println("Потребуется дней: " + amountOfDays);
 
+        }else{
+            System.out.println("Доставки нет");
+
+        }
     }
-    public static void calculationOfDaysForDelivery(int deliveryDistance){
+    public static int calculationOfDaysForDelivery(int deliveryDistance){
         int daye = 0;
         if (deliveryDistance <= 20){
             daye += 1;
-            System.out.println("Потребуется дней: " + daye);;
         }else if(deliveryDistance > 20 && deliveryDistance <= 60){
             daye += 2;
-            System.out.println("Потребуется дней: " + daye);;
         }else if (deliveryDistance > 60 && deliveryDistance <= 100) {
             daye += 3;
-            System.out.println("Потребуется дней: " + daye);;
         }else if (deliveryDistance > 100){
-            System.out.println("Доставки нет");
+            daye = -1;
         }
+        return daye;
 
 
 
